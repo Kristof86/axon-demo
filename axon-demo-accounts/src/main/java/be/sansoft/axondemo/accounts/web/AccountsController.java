e310809b157ca86dff6100dd680458ebe15e99b5
Kristof Ennekens <kristof.ennekens@gmail.com>
2021-05-22 15:50:38 +0200

    stomjs

--- axon-demo-accounts/src/main/java/be/sansoft/axondemo/accounts/web/AccountsController.java
+++ axon-demo-accounts/src/main/java/be/sansoft/axondemo/accounts/web/AccountsController.java
@@ -1,11 +1,8 @@
 package be.sansoft.axondemo.accounts.web;
 
-import be.sansoft.axondemo.accounts.domain.commands.ChangeNameCommand;
 import be.sansoft.axondemo.accounts.domain.commands.DeleteAccountCommand;
-import be.sansoft.axondemo.accounts.view.projection.details.AccountDetails;
 import be.sansoft.axondemo.accounts.view.projection.details.AccountDetailsEntity;
 import be.sansoft.axondemo.accounts.view.projection.overview.AccountsOverviewEntity;
-import be.sansoft.axondemo.accounts.view.projection.overview.AccountsOverviewRow;
 import be.sansoft.axondemo.accounts.view.query.FindAccountDetailsByIdQuery;
 import be.sansoft.axondemo.accounts.view.query.FindAllAccountsQuery;
 import be.sansoft.axondemo.accounts.web.request.ChangeNameRequest;
@@ -15,15 +12,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.gateway.CommandGateway;
-import org.axonframework.extensions.reactor.queryhandling.gateway.ReactorQueryGateway;
 import org.axonframework.queryhandling.QueryGateway;
-import org.axonframework.queryhandling.SubscriptionQueryResult;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
 
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
@@ -33,17 +28,14 @@ import java.util.concurrent.Future;
 @Slf4j
 @RestController
 public class AccountsController {
-
-    private final ReactorQueryGateway reactorQueryGateway;
+    private final SimpMessagingTemplate simpMessagingTemplate;
     private final CommandGateway commandGateway;
     private final QueryGateway queryGateway;
-    private final ObjectMapper objectMapper;
 
-    public AccountsController(ReactorQueryGateway reactorQueryGateway, CommandGateway commandGateway, QueryGateway queryGateway, ObjectMapper objectMapper) {
-        this.reactorQueryGateway = reactorQueryGateway;
+    public AccountsController(SimpMessagingTemplate simpMessagingTemplate, CommandGateway commandGateway, QueryGateway queryGateway) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
         this.commandGateway = commandGateway;
         this.queryGateway = queryGateway;
-        this.objectMapper = objectMapper;
     }
 
     @PostMapping("/createAccount")
@@ -69,18 +61,39 @@ public class AccountsController {
         return ResponseEntity.ok(future);
     }
 
-    @MessageMapping("accounts.detail")
-    public Flux<AccountDetails> all(FindAccountDetailsRequest request) {
-        return reactorQueryGateway.subscriptionQuery(new FindAccountDetailsByIdQuery(request.getId()), AccountDetailsEntity.class)
-                .map(AccountDetailsEntity::getData);
+    @SendTo("/topic/account/detail")
+    @MessageMapping("/account/detail")
+    public void detail(FindAccountDetailsRequest request) {
+        queryGateway
+                .subscriptionQuery(new FindAccountDetailsByIdQuery(request.getId()), AccountDetailsEntity.class, AccountDetailsEntity.class)
+                .handle(details -> {
+                        simpMessagingTemplate.convertAndSend(
+                                "/topic/accounts/" + request.getId() + "/details",
+                                details.getData()
+                        );
+                },
+                details -> {
+                    simpMessagingTemplate.convertAndSend(
+                            "/topic/accounts/" + request.getId() + "/details",
+                            details.getData()
+                    );
+                });
     }
 
-    @MessageMapping("accounts.all")
-    public Flux<List<AccountsOverviewRow>> all() {
-        QueryGateway queryGateway = null;
-        SubscriptionQueryResult<AccountsOverviewEntity, AccountsOverviewEntity> accountsOverviewEntityAccountsOverviewEntitySubscriptionQueryResult = queryGateway.subscriptionQuery(new FindAllAccountsQuery(), AccountsOverviewEntity.class, AccountsOverviewEntity.class);
-        return reactorQueryGateway.subscriptionQuery(new FindAllAccountsQuery(), AccountsOverviewEntity.class).map(entity -> entity.getData().getRows());
-
+    public void all() {
+        queryGateway.subscriptionQuery(new FindAllAccountsQuery(), AccountsOverviewEntity.class, AccountsOverviewEntity.class)
+                .handle(overview -> {
+                            simpMessagingTemplate.convertAndSend(
+                                    "/topic/accounts/all",
+                                    overview.getData().getRows()
+                            );
+                        },
+                        overview -> {
+                            simpMessagingTemplate.convertAndSend(
+                                    "/topic/accounts/all",
+                                    overview.getData().getRows()
+                            );
+                        });
     }
 
     /*
